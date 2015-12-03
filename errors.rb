f72b328881289f7a7a145b47654cae595e7f59ab
Katie Meyer <katiemeyer@icloud.com>
2015-12-03 00:19:46 -0600

    Add errors.rb file

---
+++ errors.rb
@@ -0,0 +1,170 @@
+# Analyze the Errors
+
+# I worked on this challenge [by myself, with: ].
+# I spent [#] hours on this challenge.
+
+# --- error -------------------------------------------------------
+
+#"Screw you guys " + "I'm going home." = cartmans_phrase
+
+# This error was analyzed in the README file.
+# --- error -------------------------------------------------------
+
+def cartman_hates(thing)
+  while true
+    puts "What's there to hate about #{thing}?"
+end
+
+# This is a tricky error. The line number may throw you off.
+
+# 1. What is the name of the file with the error?
+#errors.rb
+# 2. What is the line number where the error occurs?
+#8
+# 3. What is the type of error message?
+#syntax error
+# 4. What additional information does the interpreter provide about this type of error?
+#The '=' was unexpected, it was expecting end of input.
+# 5. Where is the error in the code?
+#The equals sign between "I'm goign home" and cartmans_phrase
+# 6. Why did the interpreter give you this error?
+#The variable was placed at the end rather than at the beginning with the equals sign after it.
+
+# --- error -------------------------------------------------------
+
+south_park
+
+# 1. What is the line number where the error occurs?
+#
+# 2. What is the type of error message?
+#
+# 3. What additional information does the interpreter provide about this type of error?
+#
+# 4. Where is the error in the code?
+#
+# 5. Why did the interpreter give you this error?
+#
+
+# --- error -------------------------------------------------------
+
+cartman()
+
+# 1. What is the line number where the error occurs?
+#
+# 2. What is the type of error message?
+#
+# 3. What additional information does the interpreter provide about this type of error?
+#
+# 4. Where is the error in the code?
+#
+# 5. Why did the interpreter give you this error?
+#
+
+# --- error -------------------------------------------------------
+
+def cartmans_phrase
+  puts "I'm not fat; I'm big-boned!"
+end
+
+cartmans_phrase('I hate Kyle')
+
+# 1. What is the line number where the error occurs?
+#
+# 2. What is the type of error message?
+#
+# 3. What additional information does the interpreter provide about this type of error?
+#
+# 4. Where is the error in the code?
+#
+# 5. Why did the interpreter give you this error?
+#
+
+# --- error -------------------------------------------------------
+
+def cartman_says(offensive_message)
+  puts offensive_message
+end
+
+cartman_says
+
+# 1. What is the line number where the error occurs?
+#
+# 2. What is the type of error message?
+#
+# 3. What additional information does the interpreter provide about this type of error?
+#
+# 4. Where is the error in the code?
+#
+# 5. Why did the interpreter give you this error?
+#
+
+
+
+# --- error -------------------------------------------------------
+
+def cartmans_lie(lie, name)
+  puts "#{lie}, #{name}!"
+end
+
+cartmans_lie('A meteor the size of the earth is about to hit Wyoming!')
+
+# 1. What is the line number where the error occurs?
+#
+# 2. What is the type of error message?
+#
+# 3. What additional information does the interpreter provide about this type of error?
+#
+# 4. Where is the error in the code?
+#
+# 5. Why did the interpreter give you this error?
+#
+
+# --- error -------------------------------------------------------
+
+5 * "Respect my authoritay!"
+
+# 1. What is the line number where the error occurs?
+#
+# 2. What is the type of error message?
+#
+# 3. What additional information does the interpreter provide about this type of error?
+#
+# 4. Where is the error in the code?
+#
+# 5. Why did the interpreter give you this error?
+#
+
+# --- error -------------------------------------------------------
+
+amount_of_kfc_left = 20/0
+
+
+# 1. What is the line number where the error occurs?
+#
+# 2. What is the type of error message?
+#
+# 3. What additional information does the interpreter provide about this type of error?
+#
+# 4. Where is the error in the code?
+#
+# 5. Why did the interpreter give you this error?
+#
+
+# --- error -------------------------------------------------------
+
+require_relative "cartmans_essay.md"
+
+# 1. What is the line number where the error occurs?
+#
+# 2. What is the type of error message?
+#
+# 3. What additional information does the interpreter provide about this type of error?
+#
+# 4. Where is the error in the code?
+#
+# 5. Why did the interpreter give you this error?
+#
+
+
+# --- REFLECTION -------------------------------------------------------
+# Write your reflection below as a comment.
