73f96f6142e8744fd7878862ffdba906687a98dd
Katie Meyer <katiemeyer@icloud.com>
2015-12-02 23:48:22 -0600

    Add files for 4.3 Variables and Methods

---
+++ week-4/define-method/my_solution.rb
@@ -0,0 +1,9 @@
+# Define an Empty Method
+def do_something( , , )
+end
+
+puts do_something( , , )
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
---
+++ week-4/math/my_solution.rb
@@ -0,0 +1,27 @@
+# Math Methods
+
+# I worked on this challenge by myself.
+
+
+# Your Solution Below
+
+def add(num_1, num_2)
+ num_1+num_2
+end
+
+def subtract(num_1, num_2)
+ num_1-num_2
+end
+
+def multiply(num_1, num_2)
+ num_1*num_2
+end
+
+def divide(num_1, num_2)
+ num_1/num_2
+end
+
+add(3,2)
+subtract(3,2)
+multiply(3,2)
+divide(3,2)
