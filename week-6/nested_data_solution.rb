af772ef3e88dafa4e5282427e87fa5964eb2bd64
Katie Meyer <katiemeyer@icloud.com>
2015-12-16 00:45:06 -0600

    Add reflection questions

--- week-6/nested_data_solution.rb
+++ week-6/nested_data_solution.rb
@@ -45,3 +45,18 @@ number_array = [5, [10, 15], [20,25,30], 35]
 # Bonus:
 
 startup_names = ["bit", ["find", "fast", ["optimize", "scope"]]]
+
+
+# Reflection
+=begin
+
+What are some general rules you can apply to nested arrays?
+-
+
+What are some ways you can iterate over nested arrays?
+-
+
+Did you find any good new methods to implement or did you re-use one you were already familiar with? What was it and why did you decide that was a good option?
+-
+
+=end
