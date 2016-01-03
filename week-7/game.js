e38d6a0a36a1c0a21eb46f3d9639a4c43b464931
Katie Meyer <katiemeyer@icloud.com>
2016-01-02 19:16:39 -0600

    Create file for 7.7 Design a Basic Game

---
+++ week-7/game.js
@@ -0,0 +1,47 @@
+// Design Basic Game Solo Challenge
+
+// This is a solo challenge
+
+// Your mission description:
+// Overall mission:
+// Goals:
+// Characters:
+// Objects:
+// Functions:
+
+// Pseudocode
+//
+//
+//
+//
+//
+
+// Initial Code
+
+
+
+
+
+
+// Refactored Code
+
+
+
+
+
+
+// Reflection
+/*
+What was the most difficult part of this challenge?
+-
+
+What did you learn about creating objects and functions that interact with one another?
+-
+
+Did you learn about any new built-in methods you could use in your refactored solution? If so, what were they and how do they work?
+-
+
+How can you access and manipulate properties of objects?
+-
+
+*/
