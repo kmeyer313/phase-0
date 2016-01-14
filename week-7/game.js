139e294765d6ba5d44b4783003ee58be29849083
Katie Meyer <katiemeyer@icloud.com>
2016-01-13 23:59:06 -0600

    Update 7.7 game

--- week-7/game.js
+++ week-7/game.js
@@ -4,201 +4,335 @@
 
 // Your mission description:
 // Overall mission: Get the most matches/points
-// Goals: match like cards in your hand; match like cards from opponent's hand
-// Characters: 2 players
-// Objects: deck of cards, 2 players
+// Goals: match 4 like cards in your hand
+// Characters: 2-4 players
+// Objects: 2 players (user and computer)
 // Functions: deal cards; identify two cards that match in hand; draw card from deck; ask other player for match; opponent checks hand; point counters; game over
 
 // Pseudocode
-// Create deck of cards collection, two of each card (object) deck
-// Create player 1 and 2 (objects) player1 player2
+// 1) Create deck of cards collection, four of each card (array) var deck
+// 2) Create player 1 and 2 (objects) var player1, player2
 //    - each property has a value of 0 to begin
-// Initiate new game on deal (function) newGame
-//    - players get 6 random cards from deck
-// Display hand (function) displayHand
-//    - display collection for player
-// Check match to see if there are matches in hand (function) checkMatch
-//    - if match, remove from hand
-//        - add point for that player
-//    - else do nothing
-// Ask opponent if they have a certain card (function) askOpponent
-//    - prompt user input
-//    - if user input is equal to 1 in opponents hand
-//        -prompt "It's a match!"
-//        - remove card from each players' hand
-//        - add point for player that received a match
-//    - else
-//        - prompt "Go fish!"
-//        - player receives a new card from deck
-//            - if matches card in deck
-//                - remove from deck
-//                - add point for player that recieved match
-// Add points for matches (function within object?) pointCounter
-//    - increase by 1 when there is a match
-// Add new card from deck to player's hand (function) goFish
-//    - select item from deck and add to hand
-// Notify when game is over (function) gameOver
-//    - when no cards left in deck, player1, or player2 hand
-//    - prompt winner (player with most points)
+// 3) Shuffle deck of cards (function) shuffle
+//    Input: deck array
+//    Output: shuffled deck array
+//    Steps:
+//      - set three variables, one for length of array
+//      - set one variable equal to random number multiplied by array length, round it down
+//      - assign and reassign variables to swap values of the new number and the original number indexed by array length value
+//      - return the shuffled array
+// 4) Deal 5 cards to each player (function) deal
+//    Input: deck array
+//    Output: 2 arrays (player1, player2)
+//    Steps:
+//      - remove 5 elements from deck array and move into new player arrays
+
+// 5) For each item in array, where it matches a property of the players' hands objects, += 1 to the value
+// ^^^ This is where I am having the most trouble ^^^ (As well as with the "turn" function a few lines down)
+//    Input: player arrays
+//    Output: player objects where values have been added to corresponding properties from arrays (flounder: 1)
+//    Steps:
+//    - cards go from array to player object, increasing value of property for 1 of each card that matches a property in player's hand
+        // I want to iterate through array and iterate through property name in object,
+        // when there is a match (==) then += to that property's value
+
+
+// 6) Check match to see if there are matches in hand (function) checkMatch
+//    Input: a player's hand (object)
+//    Output: return player's hand updated
+//    Steps:
+//      - if a property has a value of 4
+//        - set that property's value to 0
+//        - add 1 to point
+//      - else do nothing
+//      - print object to console
+// 7) Display hand (function) displayHand
+//    Input: player's deck object
+//    Output: player's deck object
+//    - print to console the player's deck
+
+// 8) Ask opponent if they have a certain card (function) turn  // Also struggled here because it requires similar logic as for loop above
+//     Input:
+//     Output:
+//     Steps:
+//      - prompt user input for what card they want to ask opponent for
+//      - if opponent has 1 or more of card in their deck that matches the user input
+//        - print to console "Yes, I do."
+//        - reassign property's value to zero for opponent
+//        - reassign property's value to += 1 for user
+//      - else
+//        - print to console "Go fish!"
+
+// 9) Add new card from deck to player's hand (function) goFish // again, same problem with moving an array item to integer value in object's matching property
+//    Input: deck and a player's hand
+//    Output: player's hand updated
+//    Steps:
+//      - select item from deck and add to hand
+//      - increase value of matching property += 1
+
+// 10) Notify when game is over (function) gameOver
+//    Input: deck, and both players' hands?
+//    Output: false or true with winner
+//    Steps:
+//      - when no cards left in deck, player1, or player2 hand
+//      - print to console winner (player with most points)
 
 
 // Initial Code
-// another way to look at the deck
-// var deck = {
-//   flounder: 2,
-//   catfish: 2,
-//   sunfish: 2,
-//   angelfish: 2,
-//   flying_fish: 2,
-//   whale: 2,
-//   dolphin: 2,
-//   goldfish: 2,
-//   shark: 2,
-//   sailfish: 2,
-// }
+// 1) Create deck of cards collection, four of each card (array) var deck
+var deck = ["flounder", "flounder", "flounder", "flounder", "catfish", "catfish", "catfish", "catfish", "sunfish", "sunfish", "sunfish", "sunfish", "angelfish", "angelfish", "angelfish", "angelfish", "flying_fish", "flying_fish", "flying_fish", "flying_fish", "whale", "whale", "whale", "whale", "dolphin", "dolphin", "dolphin", "dolphin", "goldfish", "goldfish", "goldfish", "goldfish", "shark", "shark", "shark", "shark", "sailfish", "sailfish", "sailfish", "sailfish"];
 
-// Create deck of cards collection, two of each card (object) deck
-var deck = ["flounder", "flounder", "catfish", "catfish", "sunfish", "sunfish", "angelfish", "angelfish", "flying_fish", "flying_fish", "whale", "whale", "dolphin", "dolphin", "goldfish", "goldfish", "shark", "shark", "sailfish", "sailfish"];
-
-// Create player 1 and 2 (objects) player1 player2
+// 2) Create player 1 and 2 (objects) var player1, player2
+//    - make Player class or create with literal constructor?
 //    - each property has a value of 0 to begin
+
+function Player(){
+  this.flounder = 0;
+  this.catfish = 0;
+  this.sunfish = 0;
+  this.angelfish = 0;
+  this.flying_fish = 0;
+  this.whale = 0;
+  this.dolphin = 0;
+  this.goldfish = 0;
+  this.shark = 0;
+  this.sailfish = 0;
+  this.points = 0;
+};
+
+var player1 = new Player();
+var player2 = new Player();
+
+// console.log(player1); // This is printing "Player {...}"" to console, why not "player1 {...}"??
+// console.log(player2); // Ditto ^^^
+
+// Literal contstruction of objects // Is this better? Did I do something wrong with the class above?
 // var player1 = newObject();
 // var player1 = {
-//   "flounder": 0,
-//   "catfish": 0,
-//   "sunfish": 0,
-//   "angelfish": 0,
-//   "flying_fish": 0,
-//   "whale": 0,
-//   "dolphin": 0,
-//   "goldfish": 0,
-//   "shark": 0,
-//   "sailfish": 0,
-//   matches: 0,
+//   flounder: 0,
+//   catfish: 0,
+//   sunfish: 0,
+//   angelfish: 0,
+//   flying_fish: 0,
+//   whale: 0,
+//   dolphin: 0,
+//   goldfish: 0,
+//   shark: 0,
+//   sailfish: 0,
+//   points: 0,
 // }
 
 // var player2 = newObject();
 // var player2 = {
-//   "flounder": 0,
-//   "catfish": 0,
-//   "sunfish": 0,
-//   "angelfish": 0,
-//   "flying_fish": 0,
-//   "whale": 0,
-//   "dolphin": 0,
-//   "goldfish": 0,
-//   "shark": 0,
-//   "sailfish": 0,
-//   matches: 0,
+//   flounder: 0,
+//   catfish: 0,
+//   sunfish: 0,
+//   angelfish: 0,
+//   flying_fish: 0,
+//   whale: 0,
+//   dolphin: 0,
+//   goldfish: 0,
+//   shark: 0,
+//   sailfish: 0,
+//   points: 0,
 // }
 
-// Initiate new game on deal (function) newGame
-// Input: list of cards
-// Output: player objects (list of 5 cards each; like variable objects above-- "item": occurrance)
-// Steps:
-//    - shuffle deck (create function for that)
-//    - players get 5 cards from deck (select from array, remove from array)
-//    - cards go from array to player object, increasing value of property for 1 of each card that matches a property in player's hand
-function shuffle(array) { // found this code to shuffle an array from https://www.frankmitchell.org/2015/01/fisher-yates/
-  var i = 0
-    , j= 0
-    , temp = null
-  for (i = array.length - 1; i > 0; i -= 1) {
-    j = Math.floor(Math.random() * (i + 1))
-    temp = array[i]
-    array[i] = array[j]
-    array[j] = temp
+
+// 3) Shuffle deck of cards (function) shuffle
+//    Input: deck array
+//    Output: shuffled deck array
+//    Steps:
+//      - set three variables, one for length of array
+//      - set one variable equal to random number multiplied by array length, round it down
+//      - assign and reassign variables to swap values of the new number and the original number indexed by array length value
+//      - return the shuffled array
+function shuffle(array) {
+  var m = array.length, t, i;
+  // While there remain elements to shuffle...
+  while (m) {
+    // Pick a remaining element...
+    i = Math.floor(Math.random() * m--);
+    // And swap it with the current element.
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
   }
+  return array;
 }
 shuffle(deck);
 console.log(deck);
-// console.log(deck); // prints same array as previous
+// shuffle(deck);
+// console.log(deck);
+
+
+// 4) Deal 5 cards to each player (function) deal
+//    Input: deck array
+//    Output: 2 arrays (player1, player2)
+//    Steps:
+//      - remove 5 elements from deck array and move into new player arrays
+
+var player1arr = []; // Are these necessary to be accessed outside of deal function?
+var player2arr = [];
+
+function deal(deck) {
+  // shuffle()
+  player1arr = deck.splice(0,5);
+  player2arr = deck.splice(0,5);
+}
+
+deal(deck);
+console.log(player1arr);
+console.log(player2arr);
+console.log(deck); // before this printed the deck less the 10 items I spliced out... running it again later and it printed the array with all of the cards... Not sure what changed.
 
 
 // STUCK HERE -- trying to push 5 elements from deck array into player objects where the property is the card ("sunfish", "whale") and the value is the amount of occurances.
-var player1 = {};
-var player2 = {};
-function toObject(array) {
-  for (var i = 0; i < array.length; ++i)
-    // player1[i] = array[i]; // this gives me an object, but the key/values are the reverse of how I need them
-    // return player1
-
-    // 5 times
-    // grab item from array (will no longer be in original array) and make it a key with value of 1
-    //   if item occurs more than once, increase value by 1
-  return player1;
-    // repeat to return player2
+// 5) For each item in array, where it matches a property of the players' hands objects, += 1 to the value
+// ^^^ This is where I am having the most trouble ^^^ (As well as with the "turn" function a few lines down)
+//    Input: player arrays
+//    Output: player objects where values have been added to corresponding properties from arrays (flounder: 1)
+//    Steps:
+//    - cards go from array to player object, increasing value of property for 1 of each card that matches a property in player's hand
+        // I want to iterate through array and iterate through property name in object,
+        // when there is a match (==) then += to that property's value
+// Should look something like this:
+// var player1 = newObject();
+var player1 = {
+  flounder: 0,
+  catfish: 2,
+  sunfish: 0,
+  angelfish: 0,
+  flying_fish: 1,
+  whale: 0,
+  dolphin: 1,
+  goldfish: 0,
+  shark: 1,
+  sailfish: 4,
+  points: 0,
 }
 
-toObject(deck);
-console.log(player1);
+// var player2 = newObject();
+var player2 = {
+  flounder: 1,
+  catfish: 0,
+  sunfish: 2,
+  angelfish: 2,
+  flying_fish: 0,
+  whale: 0,
+  dolphin: 0,
+  goldfish: 0,
+  shark: 0,
+  sailfish: 0,
+  points: 0,
+}
 
-// function deal_p1(array) {
-//   var array1 = array.pop(5)
-//   console.log(array1)
+// var player1 = {};
+// var player2 = {};
+// How to increase value of properties based on what was spliced to each player's hand?
+// Nothing here is functional, yet.
+// function toObject(array, object) {
+
+//   var arrayLength = array.length;
+//   for (var i=0; i<arrayLength; i++) {
+//     if (array[i] == object)
+//   }
+
+
+//   for (var j = 0; )
+//   for (var i in array) {
+//     if i == object[j]
+//   }
+//   // for (var i = 0; i < array.length; ++i)
+//     // player1[i] = array[i]; // this gives me an object, but the key/values are the reverse of how I need them
+//     // return player1
+//   return player1;
+//     // repeat to return player2
 // }
 
-// deal_p1(deck);
 
+// 6) Check match to see if there are matches in hand (function) checkMatch
+//    Input: a player's hand (object)
+//    Output: return player's hand updated
+//    Steps:
+//      - if a property has a value of 4
+//        - print to console "Match!"
+//        - set that property's value to 0
+//        - add 1 to point
+//      - else do nothing
+//      - print object to console
 
-// Display hand (function) displayHand
-//    - display collection for player
-function display(player) {
-  console.log(player);
+function checkMatch(playerObject) {
+  for (var property in playerObject) {
+    if (playerObject[property] == 4) {
+      console.log("Match!");
+      playerObject[property] = 0;
+      playerObject["points"] += 1;
+    }
+  };
+  console.log(playerObject);
+};
+
+checkMatch(player1);  // changed player1 sailfish property value to 4 above for testing
+
+// 7) Display hand (function) displayHand
+//    Input: player's deck object
+//    Output: player's deck object
+//    - print to console the player's deck
+function display(playerObject) {
+  console.log(playerObject);
 }
 
-display(player1);
-display(player2);
+// display(player1);
+// display(player2);
 
-// Check match to see if there are matches in hand (function) checkMatch
-// Input: array
-// Output: if match, array with less items and added point to score
-// Steps:
-//    - if match, remove from hand
-//        - add point for that player
-//    - else do nothing
-function checkMatch(player) {
-  if
 
-},
+// 8) Ask opponent if they have a certain card (function) turn  // Also struggled here because it requires similar logic as for loop above
+//     Input:
+//     Output:
+//     Steps:
+//      - prompt user input for what card they want to ask opponent for
+//      - if opponent has 1 or more of card in their deck that matches the user input
+//        - print to console "Yes, I do."
+//        - reassign property's value to -= 1 for opponent
+//        - reassign property's value to += 1 for user
+//      - else
+//        - print to console "Go fish!"
 
-// Ask opponent if they have a certain card (function) askOpponent
-// Input: string from opponent
-// Output: "It's a match!" or "Go fish!"
-// Steps:
-//    - prompt user input
-//    - if user input is equal to 1 in opponents hand
-//        -prompt "It's a match!"
-//        - remove card from each players' hand
-//        - add point for player that received a match
-//    - else
-//        - prompt "Go fish!"
-//        - player receives a new card from deck
-//            - if matches card in deck
-//                - remove from deck
-//                - add point for player that recieved match
+// function turn(player, opponent) {
+//   var question = prompt("opponent, do you have a ______? (type one of the following that you have in your hand: flounder, catfish, sunfish, angelfish, flying_fish, whale, dolphin, goldfish, shark, sailfish)")
+//   for (property in player) {
+//     if (question is a property && property[value] >= 1) {  // This if statement needs fixing
+//       var match = prompt("It's a match!");
+//       // opponent[value] = 0;
+//       // player[value] += 1;
+//       // player[points] += 1;
+//     } else {
+//       console.log("Go fish!");
+//     }
+//   };
+// };
 
-function askOpponent() {
-  // select a card from deck, ask opponent if they have that card, if opponent doesn't have that card prompt goFish, if opponent does have that card, decrease count from each players' hands, and drawCard
-}
+// 9) Add new card from deck to player's hand (function) goFish // again, same problem with moving an array item to integer value in object's matching property
+//    Input: deck and a player's hand
+//    Output: player's hand updated
+//    Steps:
+//      - select item from deck and add to hand
+//      - increase value of matching property += 1
 
-// // Add points for matches (function within object?) pointCounter
-// //    - increase by 1 when there is a match
-function points() {
 
-}
-
-// // Add new card from deck to player's hand (function) goFish
-// //    - select item from deck and add to hand
-function goFish() {
+function goFish(deck, hand) {
+  // player picks card from deck and value is added to appropriate property
   // for every value that is decreased, draw that amount from the deck, new cards must be added to appropriate key/value
-},
+}
 
-// // Notify when game is over (function) gameOver
-// //    - when no cards left in deck, player1, or player2 hand
-// //    - prompt winner (player with most points)
+
+// 10) Notify when game is over (function) gameOver
+//    Input: deck, and both players' hands?
+//    Output: false or true with winner
+//    Steps:
+//      - when no cards left in deck, player1, or player2 hand
+//      - print to console winner (player with most points)
 function gameOver() {
-
 }
 
 // Refactored Code
@@ -211,18 +345,18 @@ function gameOver() {
 // Reflection
 
 // What was the most difficult part of this challenge?
+//  - coming up with a game
 //  - figuring out how to pull items from the array and move them into the object;
-//  - haven't gotten beyond that yet
 
 // What did you learn about creating objects and functions that interact with one another?
-//  -
-//  -
-
+//  - I have a lot to learn. I understood it when working through CodeAcademy, but creating it from scratch is still pretty hard for me.
 
 // Did you learn about any new built-in methods you could use in your refactored solution? If so, what were they and how do they work?
-//  -
-//  -
+//  - splice seemed to work initially when I wrote it in the deal function, but when I ran it later it did not work as I needed it to
 
 // How can you access and manipulate properties of objects?
-//  -
-//  -
+//  - you can access properties by using bracket or dot notation -- objectName[property] or objectName.property can be used to access
+//  - What I trip up on here is when to use quotation marks and when not to. In the checkMatch function, it wasn't working at
+// first because I had parentheses around all the values in the brackets in the code block. Then I removed them all, and it still
+// wasn't working, I couldn't access the "points" property. Then I put quotes just around "points" and the function worked! So,
+// for a variable name in a loop, no quotation marks, but for the actual property name, use quotes to access it.
