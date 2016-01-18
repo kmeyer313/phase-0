b58b2cb32cc35b84de28e244f02363a021a6c8cf
Katie Meyer <katiemeyer@icloud.com>
2016-01-17 23:35:20 -0600

    Add reflections to 9.3 and 9.5

--- week-9/ruby-review-1/ruby-review.rb
+++ week-9/ruby-review-1/ruby-review.rb
@@ -141,12 +141,12 @@ rey.deliver
 
 =begin
 What concepts did you review in this challenge?
--
+- using instance variables and attributes accessors
 
 What is still confusing to you about Ruby?
--
+- Nothing really came to light in this challenge.
 
 What are you going to study to get more prepared for Phase 1?
--
+- Passing methods between classes.
 
 =end
